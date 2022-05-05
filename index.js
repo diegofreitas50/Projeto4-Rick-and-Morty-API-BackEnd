1c2e128141aba48b62b0bb10511b49aa60f02e8a
diegofreitas50 <diegofreitas50@hotmail.com>
2022-05-04 22:48:33 -0300

    auth route login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const characterRoute = require('./src/routes/personagem.router');
 const userRoute = require('./src/routes/users.route');
+const authRoute = require('./src/routes/auth.route');
 const connecteToDatabase = require('./src/database/database');
 const cors = require('cors');
 const port = process.env.PORT || 3001;
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/characters', characterRoute);
 app.use('/users', userRoute);
+app.use('/auth', authRoute);
 
 app.listen(port, () => { console.log(`Servidor rodando na porta :${port}`) });
